f5b438c78303071f3d730c47ffe5e09f6cc17a89
c-livi <121881563+c-livi@users.noreply.github.com>
2023-01-03 11:04:20 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,32 @@
+math.randomseed(os.clock())
 _c = os.clock()
 function _parentpath(path)
-	pattern1 = "^(.+)//"
- 	pattern2 = "^(.+)\\"
-    	if (string.match(path, pattern1) == nil) then
-        	return string.match(path, pattern2)
-    	else
-        	return string.match(path, pattern1)
-    	end
+    pattern1 = "^(.+)//"
+    pattern2 = "^(.+)\\"
+    if (string.match(path, pattern1) == nil) then
+        return string.match(path, pattern2)
+    else
+        return string.match(path, pattern1)
+    end
 end
 function _main()
 	local hashset = {}
-	local _dict = _parentpath(debug.getinfo(2, "S").source:sub(2):match("(.*/)")) .. "/englishwords.txt"
-    	for line in io.lines(_dict) do
-        	local _str = string.gsub(line, "\t", '')
-        	if string.len(_str) == 5 then
-            		hashset[_str] = true
-        	end
-    	end
+    	local _dict = _parentpath(debug.getinfo(2, "S").source:sub(2):match("(.*/)")) .. "/englishwords.txt"
+    	local comp=0
+	for i=1,3 do
+		coroutine.resume(coroutine.create(function()
+	 		for line in io.lines(_dict) do
+        			local _str = string.gsub(line, "\t", '')
+        			if not hashset[_str] then
+					if string.len(_str) == 5 then
+            					hashset[_str] = true
+        				end
+				end
+    			end
+			comp=comp+1
+	 	end))
+	end
+	while comp<3 do end
     	return hashset
 end
 print(unpack(_main()), os.clock() - _c)
